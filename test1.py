314885a110aee00e8de25472b187d4b500767598
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-11-07 12:49:37 +0530

    "changes"

---
+++ test1.py
@@ -0,0 +1,19 @@
+pins = {"Mike": 1234, "Joe": 1111, "Jack": 2222}
+# print(pins.keys())
+# print(pins.items())
+# print(pins.values())
+
+# user_input = int(input("Enter a number: "))
+# print(user_input)
+
+# print(len(pins))
+
+
+# Function Example
+
+
+def calculate_age(year):
+    age = 2019 - year
+    return age
+
+
